88dfca158306011381bd900a82ef69e5d25bf9e4
Elizabeth Villalejos <nekobanpaia@gmail.com>
2020-01-09 08:59:15 -0700

    stickler fixes

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,12 +1,17 @@
 import React from 'react';
 
-const Book = ({ id, title, category, handleDelete }) => (
+const Book = ({ 
+  id, 
+  title, 
+  category, 
+  handleDelete 
+}) => (
   <tr>
     <td>{ id }</td>
     <td>{ title }</td>
     <td>{ category }</td>
     <td>
-      <button type="submit" id={ id } onClick={ handleDelete }>
+      <button type="submit" id={id} onClick={handleDelete}>
         Delete
       </button>
     </td>
