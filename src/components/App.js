8c6c68bad7ce1a386e2e53550249ee4ce9983fbe
Elizabeth Villalejos <nekobanpaia@gmail.com>
2020-01-13 12:36:52 -0700

    Implementing Header component

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../App.css';
+import MainHeader from './Header'
 import BookList from '../containers/BookList';
 import BookForm from '../containers/BookForm';
 import CategoryFilter from './CategoryFilter';
@@ -7,6 +8,7 @@ import CategoryFilter from './CategoryFilter';
 function App() {
   return (
     <div className="App">
+      <MainHeader />
       <CategoryFilter />
       <BookList />
       <BookForm />
---
+++ src/components/Header.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import core from '@emotion/core';
+import styled from '@emotion/styled'
+
+
+export const Header = styled.header`
+  height: 95px;
+  background-color: #ffffff;
+`;
+
+export const Title = styled.div`
+  width: 240px;
+  height: 37px;
+  font-family: Montserrat;
+  font-size: 30px;
+  font-weight: bold;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: normal;
+  letter-spacing: normal;
+  color: #0290ff;
+  margin: 0 0 29px 100px;
+  padding-top:29px;
+  `;
+
+const MainHeader = () => {
+  return (
+    <Header>
+      <Title>Bookstore CMS</Title>
+    </Header>
+  )
+};
+
+export default MainHeader;
